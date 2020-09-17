56766a8f4b8d58567acf8399c3a9564cd39ed182
Benny Serra <benjaserrau@gmail.com>
2020-09-17 07:05:29 -0300

    edit

--- project/RESTapi/views.py
+++ project/RESTapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse
 
-# Create your views here.
+import tensorflow as tf
+from tf.keras.models import load_model
+from tensorflow import keras
+from rest_framework.permissions import AllowAny
+
+# Load the pretrained model
+model = load_model('models\\h5\\words\\model.h5')
