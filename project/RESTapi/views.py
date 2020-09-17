871a3bbe6fc4a43a769fc18e58aee48363530160
Benny Serra <benjaserrau@gmail.com>
2020-09-17 07:24:58 -0300

    REST API added

--- project/RESTapi/views.py
+++ project/RESTapi/views.py
@@ -3,9 +3,52 @@ from rest_framework.views import APIView
 from django.http import JsonResponse
 
 import tensorflow as tf
+import numpy as np
 from tf.keras.models import load_model
 from tensorflow import keras
 from rest_framework.permissions import AllowAny
 
+# category labels
+labels = ['confident', 'unconfident', 'pos_hp', 'neg_hp', 'interested',
+          'uninsterested', 'happy', 'unhappy', 'friendly', 'unfriendly']
+
+label_dict = dict(zip(labels, range(1, len(labels) + 1)))
+
 # Load the pretrained model
-model = load_model('models\\h5\\words\\model.h5')
+model = tf.keras.load_model('models\\h5\\words\\model.h5')
+
+
+def preprocess(x, padding_shape=30):
+    return np.array([ord(i.lower() - ord('a') + 1 if i != ' ' else 0 for i in list(x))] + ([0] * (padding_shape - len(x))), dtype=int)
+
+
+def predict(d: dict, s: str, model):
+    token = preprocess(s)
+    output = model.predict(np.array([token]))
+    Id = int(tf.keras.backend.argmax(output))
+
+    for k, v in d.items():
+        if v == Id:
+            return k
+    return 'unclassified'
+
+# Classification call class
+
+
+class Classification(APIView):
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        try:
+            # text to predict
+            text = request.POST.get('word')
+
+            # calling prediction function
+            pred = predict(label_dict, model)
+            print('Prediction: {}'.format(pred))
+
+            return JsonResponse({'message': 'Prediction : ' + str(pred), 'code': 200, 'status': 'Success'})
+        except Exception as e:
+            print('email error:', e)
+            return JsonResponse({'message': 'Something went wrong', 'code': 500, 'status': 'Error', 'error': str(e)})
